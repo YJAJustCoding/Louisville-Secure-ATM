04757b902e748044777f34bfa7e7930f6836d460
Yahya <yjalma90@.com>
2021-07-28 17:38:40 -0400

    Cleaned up the project

--- CodeLouisvilleATM/Atm.cs
+++ CodeLouisvilleATM/Atm.cs
@@ -5,16 +5,11 @@ namespace CodeLouisvilleATM
     public class Atm
     {
         public int Balance { get; set; }
-        
-        //public string Username { get; set; }
-        
-        //public int Pin { get; set; }
 
         public Atm(int balance)
         {
             Balance = balance;
         }
-
         public int CheckBalance()
         {
             Console.WriteLine($"Your Balance is ${Balance} dollars.");
@@ -26,14 +21,13 @@ namespace CodeLouisvilleATM
             if (Balance - value > -1 && value > -1)
             {
                 Balance -= value;
-                Console.WriteLine($"Your Balance is now ${Balance} dollars");
+                Console.WriteLine($"Your Balance is now ${Balance} dollars");               
             }
             else
             {
                 Console.WriteLine("1.You can't withdraw more money than you have \n2.Or a negative Number.");
             }
         }
-
         public void Deposit(int value)
         { if (value > 0)
             {
@@ -42,23 +36,16 @@ namespace CodeLouisvilleATM
             }
             else
             {
-                Console.WriteLine("Please Deposit at least 1 Dollar or more");
+                Console.WriteLine("1.Please Deposit at least 1 Dollar or more \n2.Or a negative number");
             }
         }
-        // public Void StockMarketInvesting()
-        //{
-        // balance = 0;
-        //  Console.WriteLine($"And its gone!!!!! your balance is now ${Balance} )
-        //}
-        //
         public void StockMarketInvesting(int value)
         {
             if (Balance - value == 0 && value > 0)  
             {
                 Balance = 0;
                 Console.Write($"And its gone your Balance is now ${Balance}");
-            }
-            
+            }          
             // invest all but 1 dollar
             else if (Balance - value > 0 && value > 0)
             {
--- CodeLouisvilleATM/Login.cs
+++ CodeLouisvilleATM/Login.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace CodeLouisvilleATM
 {
-    class Login
+    public class Login
     {
-    }
+        public string Username { get; set; }
+
+        public int Pin { get; set; }
+
+        public Login(string username, int pin)
+            {
+                Username = username;
+
+                Pin = pin;
+            }
+        }   
 }
